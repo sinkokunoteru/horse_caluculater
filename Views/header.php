2192b4b4fab69be860dba18b7aa1180e325e5185
sinkokunoteru <zpopfj@gmail.com>
2022-08-01 22:35:52 +0900

    デザイン回収中

--- Views/header.php
+++ Views/header.php
@@ -1,10 +1,9 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet" text="text/css" href="./css/reset.css">
   <link rel="stylesheet" text="text/css" href="./css/header.css">
   <title>馬券種別確率計算くん</title>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </head>
 <header>
   <div class="header_menu">
--- Views/index.php
+++ Views/index.php
@@ -8,23 +8,70 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" text="text/css" href="./css/top_page.css">
+    <link rel="stylesheet" text="text/css" href="./css/reset.css">
     <title>馬券種別確率計算くん</title>
   </head>
   <body>
+    <!-- ヘッダー -->
     <?php include(ROOT_PATH.'Views/header.php');?>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <form method="POST" action="./index.php">
-      <p>出馬数を入力してください</p>
-      <input type="text" name="number_of_runners" id="number_of_runners">
-      <input type='submit'>
-    </form>
-    <?php foreach($probability as $key => $result): ?>
-      <p><?=$cal->buykinds[$key]?></p>
-      <p id="output_result">単純確率<br><?=$probability[$key]?></p>
-    <?php endforeach; ?>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <!-- ヘッダーここまで -->
+    <div class="contents_position">
+      <div class="tweet_ad_how_block">
+        <div class="left_contents">
+          <div class="how_to_use_box">
+            <p class="how_to_use_title">このサイトの使い方</p>
+            <p>競馬初心者の方、</p>
+            <p>競馬を毎週やっている人への</p>
+            <p>コミュニティサイト！</p>
+            <p>自分の気になるレース、</p>
+            <p>自分なりの予想をみんなで</p>
+            <p>共有して更に競馬を</p>
+            <p>楽しんでいこう!</p>
+          </div>
+          <div class="ad1">
+            <a href="https://px.a8.net/svt/ejp?a8mat=3NEVKM+7FBNEA+1JS2+6X21T" rel="nofollow">
+            <img border="0" width="400" height="300" alt="" src="https://www24.a8.net/svt/bgt?aid=220723798449&wid=001&eno=01&mid=s00000007229001162000&mc=1"></a>
+          </div>
+        </div>
+        <div class="right_contents">
+          <div class="twitter_contents">
+            <div class="twitter_descript">
+              <p id="twitter_news">新着情報&ensp;</p>
+              <p id="twitter_hash_tag_info">#@@でツイート</p>
+            </div>
+            <div class="tweet_view">
+              <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+                aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+                aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+                aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="topic_board">
+        <p id="topic_header">話題の掲示板</p>
+        <div class="topic_box">
+          <div class="topic_block">
+            <p class="topic_title">競馬に人生かけてみた</p>
+            <p class="topic_date">2022/07/24</p>
+            <a><img width="300" height="250" src="./images/uma_1.jpg"></a>
+          </div>
+          <div class="topic_block">
+            <p class="topic_title">競馬に人生かけてみた</p>
+            <p class="topic_date">2022/07/25</p>
+            <a><img width="300" height="250" src="./images/uma_2.jpg"></a>
+          </div>
+          <div class="topic_block">
+            <p class="topic_title">競馬に人生かけてみた</p>
+            <p class="topic_date">2022/07/26</p>
+            <a><img width="300" height="250" src="./images/uma_3.jpg"></a>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+    <div>
   </body>
 </html>
